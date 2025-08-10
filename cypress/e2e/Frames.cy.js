fc25243ca1935b63a34237550264230da1da6dc2
Channa96 <channa.kumara255@gmail.com>
2025-08-10 21:34:09 +0530

    Commit 10082025-3

--- cypress/e2e/Frames.cy.js
+++ cypress/e2e/Frames.cy.js
@@ -14,7 +14,9 @@ describe('HandleFrames', ()=>
         cy.frameLoaded('#courses-iframe'); // Ensure the iframe is loaded
         // Use the iframe's contents to interact with elements inside it
         cy.iframe('#courses-iframe').find('a[href*="mentorship"]').eq(0).click();
+        cy.wait(5000);
+        cy.iframe('#courses-iframe').find('h1[class*="pricing-title"]').should('be.visible');
         // Validating the number of packages in the navigated page
-        cy.iframe().find('h1[class*="pricing-title"]').should('have.length',2);
+        cy.iframe('#courses-iframe').find('h1[class*="pricing-title"]').should('have.length',2);
     });
 })
